3a42303c145a13256540be5307ab90f12997caa5
MeanaGame <77915799+MeanaGame@users.noreply.github.com>
2021-01-24 17:14:15 +0300

    uppdate

--- el.py
+++ el.py
@@ -1,19 +1,23 @@
 import pygame as pg
 
-W, H, FPS = 500, 500, 15
-x, y, rad, rad1 = W // 2, H // 2, 75, 45
+num = int(input())
+
+W, H, FPS = 300, 300, 15
+x, y = W // 2, H // 2
 pg.init()
 app = pg.display.set_mode((W, H))
-
+first_step = 0
+tap = H//num
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
-        
-    pg.draw.ellipse(app, (250, 250, 250),(50, 100, 400, 300))
-    pg.draw.ellipse(app, (0, 0, 0),(50, 100, 380, 280))
+    while num != 0:    
+        pg.draw.ellipse(app, (250, 250, 250),(0, y - y//3, H, first_step + tap), 1)
+        pg.draw.ellipse(app, (250, 250, 250),(x - x//3, 0, first_step + tap, H), 1)
+        num -= 1
 
     pg.display.update()
     pg.time.delay(FPS)
